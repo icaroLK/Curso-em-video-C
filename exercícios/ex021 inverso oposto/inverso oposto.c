fb726d24a8b2e191563fde7a6a914ef834375fd5
Ícaro Kuchanovicz <icarolima.k7@gmail.com>
2023-06-04 19:08:10 -0300

    modulo 3

--- exercícios/ex021 inverso oposto/inverso oposto.c
+++ exercícios/ex021 inverso oposto/inverso oposto.c
@@ -2,18 +2,16 @@
 #include <locale.h>
 void main(){
     setlocale(LC_ALL, "Portuguese");
-    int num;
-    float res;
-    unsigned int op;
+    float res, num;
     printf("Insira um número: ");
     fflush(stdin);
-    scanf("%i", &num);
+    scanf("%f", &num);
     if (num>0){
         res = (float)1/num;
-        printf("O inverso de %i é igual a %.2f", num, res);
+        printf("O inverso de %.0f é igual a %.4f\n\n", num, res);
     }else{
-        op = (unsigned int)num;
-        printf("O oposto de %i é igual a %u",num, op);
+        res = num * (-1);
+        printf("O oposto de %.0f é igual a %.0f\n\n",num, res);
     }
 
 
